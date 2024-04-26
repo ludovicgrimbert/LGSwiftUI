ec7c607c6ead7b0e1c45e054586e1acd5420e862
ludovic grimbert <ludovicgrimbert@orange.fr>
2024-04-26 16:03:26 +0200

    Add simple button style

--- Sources/LGSwiftUI/Styles/ButtonStyle.swift
+++ Sources/LGSwiftUI/Styles/ButtonStyle.swift
@@ -7,6 +7,29 @@
 
 import SwiftUI
 
+public struct SimpleButtonStyle: ButtonStyle {
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.lightPrimary) var lightPrimaryColor
+    @Environment(\.darkPrimary) var darkPrimaryColor
+    @Environment(\.lightText) var lightTextColor
+    @Environment(\.darkText) var darkTextColor
+    @Environment(\.mediumValue) var mediumValue
+    @Environment(\.body1) var font
+    
+    public init(maxValue: CGFloat) {
+        self.maxValue = maxValue
+    }
+    var maxValue: CGFloat
+    
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor((colorScheme == .light ? lightTextColor : darkTextColor))
+            .font(font)
+            .frame(maxWidth: maxValue, maxHeight: mediumValue, alignment: .center)
+    }
+}
+
+
 public struct PrimaryButtonStyle: ButtonStyle {
     @Environment(\.colorScheme) var colorScheme
     @Environment(\.lightPrimary) var lightPrimaryColor
@@ -28,14 +51,39 @@ public struct PrimaryButtonStyle: ButtonStyle {
             .frame(maxWidth: maxValue, maxHeight: mediumValue, alignment: .center)
             .background((colorScheme == .light ? lightPrimaryColor : darkPrimaryColor).opacity(configuration.isPressed ? 0.7 : 1))
             .clipShape(RoundedRectangle(cornerRadius: mediumValue/2))
-//            .overlay(
-//                PrimaryRectangleGradient()
-//            )
+            .scaleEffect(configuration.isPressed ? 1.1 : 1.0)
+    }
+}
+
+public struct RectangleButtonStyle: ButtonStyle {
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.lightPrimary) var lightPrimaryColor
+    @Environment(\.darkPrimary) var darkPrimaryColor
+    @Environment(\.lightText) var lightTextColor
+    @Environment(\.darkText) var darkTextColor
+    @Environment(\.mediumValue) var mediumValue
+    @Environment(\.body1) var font
+    
+    public init(maxValue: CGFloat) {
+        self.maxValue = maxValue
+    }
+    var maxValue: CGFloat
+    
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor((colorScheme == .light ? lightTextColor : darkTextColor))
+            .font(font)
+            .frame(maxWidth: maxValue, maxHeight: mediumValue, alignment: .center)
+            .border((colorScheme == .light ? lightTextColor : darkTextColor), width: 0.5)
+            .background((colorScheme == .light ? lightPrimaryColor : darkPrimaryColor).opacity(configuration.isPressed ? 0.7 : 1))
             .scaleEffect(configuration.isPressed ? 1.1 : 1.0)
     }
 }
 
 
+
+
+// REMOTE TV
 public struct CircleButtonStyle: ButtonStyle {
     @Environment(\.colorScheme) var colorScheme
     @Environment(\.lightPrimary) var lightPrimaryColor
@@ -87,26 +135,3 @@ struct PrimaryGradient: View {
     }
 }
 
-struct PrimaryRectangleGradient: View {
-    @Environment(\.largeValue) var largeValue
-
-    @Environment(\.lightAccent) var lightAccentColor
-    @Environment(\.darkAccent) var darkAccentColor
-    
-    var body: some View {
-        RoundedRectangle(cornerRadius: largeValue/2)
-            .stroke(
-                LinearGradient(
-                    gradient: Gradient(
-                        colors: [
-                            lightAccentColor,
-                            darkAccentColor
-                        ]
-                    ),
-                    startPoint: .top,
-                    endPoint: .trailing
-                ),
-                lineWidth: 1.0
-            )
-    }
-}
