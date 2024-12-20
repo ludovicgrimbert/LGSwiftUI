3c0f64b76b4131e487631b485ec83d3e1140b7cb
ludovic grimbert <ludovicgrimbert@orange.fr>
2024-12-20 18:12:20 +0100

    Clean

--- Sources/LGSwiftUI/Styles/BackgroundStyle.swift
+++ Sources/LGSwiftUI/Styles/BackgroundStyle.swift
@@ -11,14 +11,10 @@ public struct BackgroundPrimaryStyle: ViewModifier {
     public init() {}
     @Environment(\.theme) var theme
     @Environment(\.colorScheme) var colorScheme
-//    @Environment(\.lightPrimaryBackground) var lightPrimaryBackgroundColor
-//    @Environment(\.darkPrimaryBackground) var darkPrimaryBackgroundColor
     
     public func body(content: Content) -> some View {
         content
             .background(colorScheme == .light ? theme.lightPrimaryBackgroundColor : theme.darkPrimaryBackgroundColor)
-//            .background(colorScheme == .light ? lightPrimaryBackgroundColor : darkPrimaryBackgroundColor)
-
     }
 }
 
@@ -26,12 +22,9 @@ public struct BackgroundSecondaryStyle: ViewModifier {
     public init() {}
     @Environment(\.theme) var theme
     @Environment(\.colorScheme) var colorScheme
-//    @Environment(\.lightSecondaryBackground) var lightSecondaryBackgroundColor
-//    @Environment(\.darkSecondaryBackground) var darkSecondaryBackgroundColor
     
     public func body(content: Content) -> some View {
         content
             .background(colorScheme == .light ? theme.lightSecondaryBackgroundColor : theme.darkSecondaryBackgroundColor)
-//            .background(colorScheme == .light ? lightSecondaryBackgroundColor : darkSecondaryBackgroundColor)
     }
 }
