e182063e82da6af21bb0ef8cba29375b66440319
ludovic grimbert <ludovicgrimbert@orange.fr>
2025-01-08 19:06:04 +0100

    Add textfield style

--- Sources/LGSwiftUI/Styles/LabelStyle.swift
+++ Sources/LGSwiftUI/Styles/LabelStyle.swift
@@ -29,3 +29,35 @@ public struct CustomIconLabelStyle: LabelStyle {
         }
     }
 }
+
+public struct CustomTextFieldStyle: TextFieldStyle {
+    var color: Color
+    var font: Font
+    var cornerRadius: CGFloat
+    var strokeColor: Color
+    var lineWidth: CGFloat
+
+    public init(color: Color,
+                font: Font,
+                cornerRadius: CGFloat = 0,
+                strokeColor: Color = .clear,
+                lineWidth: CGFloat = 1) {
+        self.color = color
+        self.font = font
+        self.cornerRadius = cornerRadius
+        self.strokeColor = strokeColor
+        self.lineWidth = lineWidth
+
+    }
+    
+    public func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .foregroundColor(color)
+            .font(font)
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .stroke(strokeColor, lineWidth: lineWidth)
+            )
+    }
+}
